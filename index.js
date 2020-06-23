846eb292e704171006188fed701f9a3ba4b804b2
Logan Scott <logantscott@gmail.com>
2020-06-23 07:56:42 -0600

    copy & transform tests passing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fsPromises = require('fs').promises;
-const { writeFile, readFile, copy, exists } = require('./lib/fs');
+const { writeFile, readFile, copy, exists } = require('./lib/copyFile');
 
 // writeFile('./files/secondFile.txt', 'this is a test function for write');
 // readFile('./files/secondFile.txt');
 
-copy('./files/secondFile.txt', './files/thirdFile.txt');
-exists('./files/secondFile.txt');
-exists('./files/secondFile2.txt');
+// copy('./files/secondFile.txt', './files/thirdFile.txt');
+// exists('./files/secondFile.txt');
+// exists('./files/secondFile2.txt');
